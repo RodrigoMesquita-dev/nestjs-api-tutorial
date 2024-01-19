db15bbbd11f9762a24b09f41fa4ea89719f216e7
rodrigomesquita-dev <rodrigomesquita9232@gmail.com>
2024-01-19 19:01:58 -0400

    chore: prisma and controllers, 55:32 minutes of video

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  signup() {
+    return 'i am signup';
+  }
 
-    signup() {
-        return 'i am signup';
-    }
-    
-    signin() {
-        return 'i am signin';
-    }
+  signin() {
+    return 'i am signin';
+  }
 }
